e3ea613adebe1be382102d6c294790de72523088
anuragdevon <msm19b021@iiitdm.ac.in>
2021-06-27 21:16:51 +0530

    modified

---
+++ HMS/hostel/models.py
@@ -0,0 +1,89 @@
+from enum import auto
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class Session(models.Model):
+    # id = models.AutoField(primary_key=True)
+    session_start_year = models.DateField()
+    session_end_year = models.DateField()
+
+class CustomUser(AbstractUser):
+    user_type_data = ((1, "Dean"), (2, "Staff"), (3, "Student"))
+    user_type = models.CharField(default=1, choices=user_type_data, max_length=10)
+
+class AdminDean(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Staff(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    address = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Student(models.Model):
+    admin = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
+    gender = models.CharField(max_length=60)
+    address = models.TextField()
+    session_year_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+
+class Attendance(models.Model):
+    attendance_date = models.DateField()
+    session_id = models.ForeignKey(Session, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class AttendanceReport(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.DO_NOTHING)
+    attendace_id = models.ForeignKey(Attendance, on_delete=models.CASCADE)
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class LeaveReportStudent(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    # leave_date = models.CharField(max_length=255)
+    leave_date = models.DateTimeField()
+    leave_message = models.TextField()
+    leave_status = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class LeaveReportStaff(models.Model):
+    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    # leave_date = models.CharField(max_length=255)
+    leave_date = models.DateTimeField()
+    leave_message = models.TextField()
+    leave_status = models.IntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class FeedBackStudent(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feeback_reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class FeedBackStaff(models.Model):
+    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    feedback = models.TextField()
+    feeback_reply = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class NotificationStudent(models.Model):
+    student_id = models.ForeignKey(Student, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class NotificationStaffs(models.Model):
+    staff_id = models.ForeignKey(Staff, on_delete=models.CASCADE)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+# will be doing django signals
